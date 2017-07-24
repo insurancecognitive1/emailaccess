0ffcb99886b7782b0b9906b412a343d0b345730c
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-24 15:27:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+console.log('reached here');    
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
