01d86e0890232d81293a940a5fe532f321553e5b
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-24 15:25:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "Node.js Sample",
+  "description": "A barebones Node.js app using Express 4 for email access",
+  "repository": "https://github.com/insurancecognitive1/emailaccess",
+  "keywords": ["node", "express", "static"]
+}
