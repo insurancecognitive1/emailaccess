6238b8ea9b1e8f8cbeb5c33fc245c6c1e574f906
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-25 11:18:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.get('/', function(req, res) {
 console.log('reached get');   
     console.log(req.query.code);
-    var jsonreq = JSON.stringify({client_id: "c6f36595-cad5-4861-8dd7-b6849cab70bd",scope:"mail.read",code:req.query.code,client_secret:"X5gnN89guhOP6v6eyubQXwP",redirect_uri:"https://emailaccess.herokuapp.com",grant_type:"authorization_code"};
+    var jsonreq = {client_id: "c6f36595-cad5-4861-8dd7-b6849cab70bd",scope:"mail.read",code:req.query.code,client_secret:"X5gnN89guhOP6v6eyubQXwP",redirect_uri:"https://emailaccess.herokuapp.com",grant_type:"authorization_code"};
     console.log(jsonreq);
     httpreq({
     url: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
