4275a71efd24bd9b9169738a57354782b2d075ae
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-07-25 12:46:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ var client = MicrosoftGraph.Client.init({
             return;
         }
       //  console.log(res);
-        var jsonresp=JSON.parse(res).value[0];
+        var jsonresp=JSON.parse(res);
+        jsonresp=jsonresp.value[0];
         console.log('subject ',jsonresp.subject);
         console.log('bodypreview ',jsonresp.bodyPreview);
         console.log('body ',jsonresp.body);
