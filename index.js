bbbc63d25ab752130877aa30b2c3df67eb1afdd4
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-08-18 15:49:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ var bodyParser = require('body-parser');
 var httpreq = require('request');
 //var jsonreq = {"client_id": "c6f36595-cad5-4861-8dd7-b6849cab70bd","scope":"mail.read","code"="M130722b4-1c3d-72f7-2521-dada13ec9c89","client_secret"="X5gnN89guhOP6v6eyubQXwP","redirect_uri"="https://emailaccess.herokuapp.com/signin","grant_type"="authorization_code"}
 const MicrosoftGraph = require("@microsoft/microsoft-graph-client");
-var rawemail = "Test";
+var rawemail = "";
+const watson = require('watson-developer-cloud');
+const natural_language_classifier = watson.natural_language_classifier({
+  username: 'e122adbe-5489-48b8-9c6c-222ae7a72d1d',
+  password: 'ShXws2ujrNaE',
+  version: 	'v1'
+});
+var conversation = watson.conversation({
+  username: 'dccf3bfa-6e72-4589-a66d-1089784818cc',
+  password: 'lENF1ZsL51Sp',
+  version: 'v1',
+  version_date: '2017-04-21'
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -87,8 +99,55 @@ console.log('reached post');
     res.send('Hello world post');
 });
 
+//To get the raw email
 app.get('/api/rawemail', function(req, res) {
     res.send(rawemail);
+});
+
+//Calling the Conversation API services
+app.post('/api/emailclassify',function(req, res){
+
+
+  console.log("Context Body: " +req.body.context);
+  console.log("Text Body: " +req.body.text);
+
+  conversation.message({
+    workspace_id: 'aed00036-e0d1-4a47-8906-e11b16f5f9f1',
+    input: {text: req.body.text},
+    context: req.body.context,
+    alternate_intents: true
+  }, function(err, response) {
+    if (err)
+    {
+      console.log('error:', err);
+      res.send(err);
+    }
+    else
+    {
+      console.log("Complete response" +JSON.stringify(response, null, 2));
+      //var intent = JSON.stringify(response,null,2);
+      //intent = intent.result.intents[0].intent;
+      res.send( JSON.stringify(response,null,2));
+    }
+  });
+}); 
+
+//to classify teh email
+app.get('/classify',function(req,res){
+	console.log(req.query.text);
+	var text=req.query.text;
+	  natural_language_classifier.classify({ text: text ,  classifier_id: '1c5f1ex204-nlc-68213'},
+    function(err, tone) {
+      if (err)
+      {
+        console.log(err);
+        res.send(err);
+    }
+      else{
+      	console.log(JSON.stringify(tone, null, 2));
+        res.send(JSON.stringify(tone, null, 2));
+      }
+  });
 });
 
 app.listen(app.get('port'), function() {
