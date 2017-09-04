11556f5c6d0eb56a4a2ec01856806744e50c9c1b
insurancecognitive1 <insurancecognitive1@gmail.com>
2017-09-04 12:57:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -251,8 +251,8 @@ app.get('/signout', function(req, res) {
 
 app.post('/signout', function(req, res) {
     console.log('Sign Out..');
-    //res.sendFile('ui/index.html', { root : __dirname}); 
-    res.redirect('ui/index.html', { root : __dirname});
+    res.sendFile('ui/index.html', { root : __dirname}); 
+    //res.redirect('ui/index.html', { root : __dirname});
     /*var jsonreq = {post_logout_redirect_uri:"https://emailaccess.herokuapp.com/"};
     console.log(jsonreq);
     httpreq({
